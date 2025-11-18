36fbc10482f4f01d8d3675d6ea2f20c45026fe86
SuperDimaROZOVII <demonrozov916@gmail.com>
2025-11-18 21:00:13 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { favourite, load, remove } from './store/GalleryReducer';
+import { useState } from 'react';
+import { FaStar } from "react-icons/fa";
+import "./index.css";
+//bootstrap imports:
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const dispatch = useDispatch()
+  const [url, setUrl] = useState("")
+  const images = useSelector(state => state.gallery.images)
+  // const Star = ({ selected = false, onSelect = f => f }) => (
+  //   <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+  // )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" placeholder='url' value={url} onChange={e => setUrl(e.target.value)}/>
+      <button onClick={() => { dispatch(load(url)); setUrl('') }} className="btn btn-success">add Image</button>
+      <div className="container"> 
+         <div className="row align-items-start"> {/*   col-lg-4 col-md-12 mb-4 mb-lg-0  */}
+          {images.map(image => (
+            <div key={image.id} className='col-lg-4 mb-4 mb-lg-0'>
+              <p />
+              <button onClick={() => { dispatch(remove(image.id)) }} className='btn btn-danger'>Remove Image</button>
+              <p>favourited: {`${image.favourited}`}</p>
+              <FaStar color={image.favourited ? "red" : "grey"} onClick={() => { dispatch(favourite(image.id)) }} />
+              <img src={image.url} id={image.id} alt='failed to load' className="w-100 shadow-1-strong rounded mb-4"/>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </>
   );
 }
 
